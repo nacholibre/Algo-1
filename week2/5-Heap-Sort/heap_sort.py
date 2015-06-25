e2381bd3f94167fbad21722fb41f143e25dff706
Yoan Arnaudov <jonidev@gmail.com>
2015-06-25 18:09:58 +0300

    add heap sort

--- week2/5-Heap-Sort/heap_sort.py
+++ week2/5-Heap-Sort/heap_sort.py
@@ -1,59 +1,121 @@
+
 class HeapSort:
 
+    def __init__(self):
+        self.mylist = [None]
+        self.current = None
+        self.root = None
+        self.nextIndex = 1
+
     # Sorts a sequence of integers.
-    def sort(sequence):
-        pass
+    def sort(self, sequence):
+        for number in sequence:
+            self.insert(number)
 
+        sortedList = []
 
-def getParent(heap, i):
-    return heap[i/2]
+        while True:
+            popped = self.remove()
+            if not popped:
+                break
+            sortedList.append(popped)
 
+        return sortedList
 
-def swapPoVerigata():
-    pass
+    def _parent(self, index):
+        return self.mylist[index/2]
+
+    def _parentIndex(self, index):
+        return index/2
+
+    def _root(self):
+        return self.mylist[1]
+
+    def _swap(self, currentIndex, parentIndex):
+        self.mylist[currentIndex], self.mylist[parentIndex] = \
+            self.mylist[parentIndex], self.mylist[currentIndex]
+
+    def insert(self, number):
+        self.mylist.append(number)
+
+        currentIndex = self.nextIndex
+
+        while self.mylist[currentIndex] != self._root() and \
+                self.mylist[currentIndex] > self._parent(currentIndex):
+            self._swap(currentIndex, self._parentIndex(currentIndex))
+            currentIndex = self._parentIndex(currentIndex)
+
+        self.nextIndex += 1
+
+    def _getLeftChildIndex(self, index):
+        return index*2
+
+    def _getRightChildIndex(self, index):
+        return index*2 + 1
+
+    def _getLeftChild(self, parentIndex):
+        index = parentIndex*2
+
+        try:
+            return self.mylist[index]
+        except IndexError:
+            return None
+
+    def _getRightChild(self, parentIndex):
+        index = (parentIndex*2) + 1
+        try:
+            return self.mylist[index]
+        except IndexError:
+            return None
+
+    def _largerChildIndex(self, parentIndex):
+        leftChild = self._getLeftChild(parentIndex)
+        rightChild = self._getRightChild(parentIndex)
+
+        if leftChild > rightChild:
+            return parentIndex*2
+        else:
+            return (parentIndex*2) + 1
+
+    def remove(self):
+        if self.mylist[self.nextIndex - 1] is None:
+            return None
+
+        lastElement = self.mylist.pop(self.nextIndex - 1)
+        self.nextIndex -= 1
+
+        if len(self.mylist) is 1:
+            return lastElement
+
+        root = self.mylist[1]
+
+        try:
+            self.mylist[1] = lastElement
+        except IndexError:
+            return None
+
+        currentIndex = 1
+        while (self._getLeftChild(currentIndex) and self.mylist[currentIndex] < self._getLeftChild(currentIndex)) or \
+                (self._getRightChild(currentIndex) and self.mylist[currentIndex] < self._getRightChild(currentIndex)):
+            largerChildIndex = self._largerChildIndex(currentIndex)
+            self._swap(currentIndex, largerChildIndex)
+            currentIndex = largerChildIndex
+
+        return root
 
 
 if __name__ == '__main__':
-    lst = [5, 3, 10, 15, 25, 2, 12]
+    lst = [5, 3, 10, 15, 25, 2, 12, 30, 40, 45, 150, 500]
 
-    heap = [None] * 20
+    heapSort = HeapSort()
 
-    heapIndex = 1
+    print heapSort.sort(lst)
 
-    for x in range(len(lst)):
-        currentElement = lst[x]
-
-        heapIndex = x + 1
-        heap[heapIndex] = currentElement
-
-        parentIndex = heapIndex/2
-        childIndex = heapIndex
-
-        child = heap[childIndex]
-        parent = heap[parentIndex]
-
-        swapPosition = heapIndex
-
-        if parent and child > parent:
-            # swap
-            heap[childIndex] = parent
-            heap[parentIndex] = child
-            # nextParentIndex = heapIndex-1/2
-            # nextParent = heap[nextParentIndex]
-
-    print lst
-    print heap
-
-    #     pass
-
-    # heap[1] = lst.pop()
-
-    # i = 2
-    # heap[i] = lst.pop()
-
-    # parent = i/2
-
-    # def getParent(myList, i):
-    #     return myList[i/2]
-
-    # print getParent(lst, 3)
+    # heapSort.insert(2)
+    # heapSort.insert(12)
+    # heapSort.insert(30)
+    # print heapSort.mylist
+    # print 'remove: ', heapSort.remove()
+    # print heapSort.mylist
+    # heapSort.insert(30)
+    # print heapSort.mylist
--- week3/1-BST/bst.py
+++ week3/1-BST/bst.py
@@ -2,5 +2,23 @@ class BST:
 
     # Checks if a binary tree is a binary search tree.
     # root - node with `left`, `right` and `value` properties
-    def isBST(root):
+    def isBST(self, root):
         pass
+
+
+class Node:
+    def __init__(self):
+        self.value = None
+        self.left = None
+        self.right = None
+
+
+if __name__ == '__main__':
+    mylist = [15, 20, 1, 30, 40, 100, 6, 3]
+
+    currentIndex = 0
+
+    node = Node()
+    node.value = mylist.pop(0)
+
+    root = node
